916bd43a01488c30c04e58a05fdf391a543a22d5
Template Janitor <template.janitor@example.com>
2023-06-20 20:55:33 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -4,7 +4,7 @@ include(CMakeDependentOption)
 include(CheckCXXCompilerFlag)
 
 
-macro(myproject_supports_sanitizers)
+macro(OHNavi_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
     set(SUPPORTS_UBSAN ON)
   else()
@@ -18,183 +18,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(OHNavi_setup_options)
+  option(OHNavi_ENABLE_HARDENING "Enable hardening" ON)
+  option(OHNavi_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    OHNavi_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    OHNavi_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  OHNavi_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR OHNavi_PACKAGING_MAINTAINER_MODE)
+    option(OHNavi_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(OHNavi_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(OHNavi_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(OHNavi_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(OHNavi_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(OHNavi_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(OHNavi_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(OHNavi_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(OHNavi_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(OHNavi_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(OHNavi_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(OHNavi_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(OHNavi_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(OHNavi_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(OHNavi_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(OHNavi_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(OHNavi_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(OHNavi_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(OHNavi_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(OHNavi_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(OHNavi_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(OHNavi_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(OHNavi_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(OHNavi_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(OHNavi_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(OHNavi_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      OHNavi_ENABLE_IPO
+      OHNavi_WARNINGS_AS_ERRORS
+      OHNavi_ENABLE_USER_LINKER
+      OHNavi_ENABLE_SANITIZER_ADDRESS
+      OHNavi_ENABLE_SANITIZER_LEAK
+      OHNavi_ENABLE_SANITIZER_UNDEFINED
+      OHNavi_ENABLE_SANITIZER_THREAD
+      OHNavi_ENABLE_SANITIZER_MEMORY
+      OHNavi_ENABLE_UNITY_BUILD
+      OHNavi_ENABLE_CLANG_TIDY
+      OHNavi_ENABLE_CPPCHECK
+      OHNavi_ENABLE_COVERAGE
+      OHNavi_ENABLE_PCH
+      OHNavi_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  OHNavi_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (OHNavi_ENABLE_SANITIZER_ADDRESS OR OHNavi_ENABLE_SANITIZER_THREAD OR OHNavi_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(OHNavi_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(OHNavi_global_options)
+  if(OHNavi_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    OHNavi_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  OHNavi_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(OHNavi_ENABLE_HARDENING AND OHNavi_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR OHNavi_ENABLE_SANITIZER_UNDEFINED
+       OR OHNavi_ENABLE_SANITIZER_ADDRESS
+       OR OHNavi_ENABLE_SANITIZER_THREAD
+       OR OHNavi_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${OHNavi_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${OHNavi_ENABLE_SANITIZER_UNDEFINED}")
+    OHNavi_enable_hardening(OHNavi_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(OHNavi_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(OHNavi_warnings INTERFACE)
+  add_library(OHNavi_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  OHNavi_set_project_warnings(
+    OHNavi_warnings
+    ${OHNavi_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(OHNavi_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    configure_linker(myproject_options)
+    configure_linker(OHNavi_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  OHNavi_enable_sanitizers(
+    OHNavi_options
+    ${OHNavi_ENABLE_SANITIZER_ADDRESS}
+    ${OHNavi_ENABLE_SANITIZER_LEAK}
+    ${OHNavi_ENABLE_SANITIZER_UNDEFINED}
+    ${OHNavi_ENABLE_SANITIZER_THREAD}
+    ${OHNavi_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(OHNavi_options PROPERTIES UNITY_BUILD ${OHNavi_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(OHNavi_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      OHNavi_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(OHNavi_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    OHNavi_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(OHNavi_ENABLE_CLANG_TIDY)
+    OHNavi_enable_clang_tidy(OHNavi_options ${OHNavi_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(OHNavi_ENABLE_CPPCHECK)
+    OHNavi_enable_cppcheck(${OHNavi_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(OHNavi_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    OHNavi_enable_coverage(OHNavi_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(OHNavi_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(OHNavi_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(OHNavi_ENABLE_HARDENING AND NOT OHNavi_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR OHNavi_ENABLE_SANITIZER_UNDEFINED
+       OR OHNavi_ENABLE_SANITIZER_ADDRESS
+       OR OHNavi_ENABLE_SANITIZER_THREAD
+       OR OHNavi_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    OHNavi_enable_hardening(OHNavi_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,5 +1,5 @@
 function(
-  myproject_enable_sanitizers
+  OHNavi_enable_sanitizers
   project_name
   ENABLE_SANITIZER_ADDRESS
   ENABLE_SANITIZER_LEAK
